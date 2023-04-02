4adcded1be0d34c19746ab00a074f69d15bddd0c
Adan Rocha <adanrocha2014@gmail.com>
2023-04-02 00:52:02 -0300

    Exercícios - Tipos e variáveis

---
+++ Tipos-Variaveis/src/App.java
@@ -0,0 +1,12 @@
+public class App
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int a = 1; //Primeiro número a ser somado
+        int b = 2; //Segundo número
+        int soma = a + b; //resultado da soma
+
+        System.out.println("Hello, World! My name is Machine."); //boas vindas
+        System.out.println("A soma de seus números escolhidos é: " + (soma)); // resultado da adição
+    }
+}
